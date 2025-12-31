809cf5c300912702a961bc8b2b15a86af7e72fe2
Richard_ckz <98233098+Richardckz@users.noreply.github.com>
2025-12-31 03:47:07 +0100

    personalProject/

--- repository/ExperienceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Siw.personalProject.repository;
-
-import com.Siw.personalProject.model.ExperienceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExperienceRepository extends JpaRepository<ExperienceEntity, Long> {
-}
